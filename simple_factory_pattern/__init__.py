ef195266494ddf9e4a982bf8bc71912fc5ff35de
mammo0 <marc.ammon@hotmail.de>
2021-06-02 10:59:05 +0200

    use relative import within the package
    -> make the package more portable

--- simple_factory_pattern/__init__.py
+++ simple_factory_pattern/__init__.py
@@ -1,8 +1,7 @@
 """
 This module provides a simple implementation of a factory pattern.
 """
-from simple_factory_pattern.factorypattern_meta import _FactoryPatternMeta, \
-    _FactoryException
+from .factorypattern_meta import _FactoryPatternMeta, _FactoryException
 
 
 __all__ = ["FactoryPatternMeta", "FactoryException"]
